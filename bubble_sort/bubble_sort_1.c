967a00abe0b93813caee7ad2b2c18e278537afc3
Pan Shengjie <perry2008084@gmail.com>
2014-03-03 23:59:48 +0800

    bug correct

--- bubble_sort/bubble_sort_1.c
+++ bubble_sort/bubble_sort_1.c
@@ -12,7 +12,7 @@ void main()
 	int i, j, tmp, number[LENGTH] = {95, 45, 15, 78, 84, 51, 24, 12};
 
 	for (i = 0; i < LENGTH; i++) {
-		for (j = LENGTH - 1; j > i; j++) {
+		for (j = LENGTH - 1; j > i; j--) {
 			if (number[j] < number[j-1]) {
 				tmp = number[j-1];
 				number[j-1] = number[j];
@@ -22,7 +22,7 @@ void main()
 	}
 
 	for (i = 0; i < LENGTH; i++) {
-		printf("%d", number[i]);
+		printf("%d ", number[i]);
 	}
 	printf("\n");
 }
